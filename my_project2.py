7bc1329abc12f0c03bfd3c0ca30054e1caf47594
Ari Widen <ariwiden@gmail.com>
2017-10-22 13:46:01 -0500

    test

--- my_project2.py
+++ my_project2.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup as soup
 my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20cards'
 
 
-uClient = uReq(my_url)
+ uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
